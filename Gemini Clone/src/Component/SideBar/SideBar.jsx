48a2face84a820111e634a07742fd1f7bbc05119
Yash-8888 <codingwithcandy@gmail.com>
2025-09-27 22:14:02 +0530

    css to side bar

--- Gemini Clone/src/Component/SideBar/SideBar.jsx
+++ Gemini Clone/src/Component/SideBar/SideBar.jsx
@@ -3,31 +3,31 @@ import {assets} from '../../assets/assets'
 
 const SideBar = () => {
   return (
-    <div className="justify-between bg-[#202123] text-white flex flex-col h-full p-2">
-        <div className="top">
-            <img src={assets.menu_icon} alt="Menu Icon" className='logo' />
-            <div className="new-chats">
+    <div className="sidebar">
+        <div className=" ">
+            <img src={assets.menu_icon} alt="Menu Icon"  />
+            <div className="flex mt-4 gap-1">
                 <img src={assets.plus_icon} alt="" />
                 <p>New Chat</p>
             </div>
-            <div className="recent">
-                <p>Recent</p>
-                <div className="recent-entry">
+            <div className="">
+                <p className='size-20'>Recent</p>
+                <div className="flex items-center gap-1">
                     <img src={assets.message_icon} alt="Chat Icon" />
                     <p>What is react  ...</p>
                 </div>
             </div>
         </div>
-        <div className="bottom">
-            <div className="bottom-item">
+        <div className=""> 
+            <div className="px-[10px] flex items-center gap-1">
                 <img src={assets.question_icon} alt="Light Mode Icon" />
                 <p>Help</p>
             </div>
-            <div className="bottom-item">
+            <div className="px-[10px] flex items-center gap-1">
                 <img src={assets.history_icon} alt="Light Mode Icon" />
                 <p>Activity</p>
             </div>
-            <div className="bottom-item">
+            <div className="px-[10px] flex items-center gap-1">
                 <img src={assets.setting_icon} alt="Light Mode Icon" />
                 <p>settings</p>
             </div>
