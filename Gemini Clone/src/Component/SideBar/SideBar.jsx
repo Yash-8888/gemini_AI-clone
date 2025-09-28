0ca1145705e975639ef1b66579e11390d3eed9ff
Yash-8888 <codingwithcandy@gmail.com>
2025-09-28 12:54:35 +0530

    sideba is ready

--- Gemini Clone/src/Component/SideBar/SideBar.jsx
+++ Gemini Clone/src/Component/SideBar/SideBar.jsx
@@ -4,30 +4,34 @@ import {assets} from '../../assets/assets'
 const SideBar = () => {
   return (
     <div className="sidebar">
-        <div className=" ">
-            <img src={assets.menu_icon} alt="Menu Icon"  />
-            <div className="flex mt-4 gap-1">
-                <img src={assets.plus_icon} alt="" />
-                <p>New Chat</p>
+        <div className="gap-13 flex flex-col">
+            <div className="meun">
+                <img src={assets.menu_icon} alt=""  />
             </div>
-            <div className="">
-                <p className='size-20'>Recent</p>
-                <div className="flex items-center gap-1">
-                    <img src={assets.message_icon} alt="Chat Icon" />
+            <div className="gap-8  flex flex-col">
+                <div className="p-[10px] justify-center h-[3rem] text-[14px] text-gray-500 font-bold rounded-full bg-gray-200 gap-[10px] mt-[10px] rounded items-center flex mt-4 gap-1 cursor-pointer ">
+                <img className='p-10' src={assets.plus_icon} alt="" />
+                <p className="">New Chat</p>
+            </div>
+            <div className="gap-10 flex flex-col items-start">
+                <p className='text-[15px] font-semibold'>Recent</p>
+                <div className="text-gray-500 text-[10px] flex items-center items-center cursor-pointer text-[14px]">
+                    <img className='text-[10pxs] ' src={assets.message_icon} alt="Chat Icon" />
                     <p>What is react  ...</p>
                 </div>
             </div>
+            </div>
         </div>
-        <div className=""> 
-            <div className="px-[10px] flex items-center gap-1">
+        <div className="h-27 flex flex-col gap-5 text-gray-800 text-[16px] place-self-center"> 
+            <div className="flex gap-3 text-[16px] hover:bg-gray-300 rounded-lg ">
                 <img src={assets.question_icon} alt="Light Mode Icon" />
                 <p>Help</p>
             </div>
-            <div className="px-[10px] flex items-center gap-1">
+            <div className="flex gap-3 hover:bg-gray-300 rounded-lg ">
                 <img src={assets.history_icon} alt="Light Mode Icon" />
                 <p>Activity</p>
             </div>
-            <div className="px-[10px] flex items-center gap-1">
+            <div className="flex gap-3 hover:bg-gray-300 rounded-lg ">
                 <img src={assets.setting_icon} alt="Light Mode Icon" />
                 <p>settings</p>
             </div>
